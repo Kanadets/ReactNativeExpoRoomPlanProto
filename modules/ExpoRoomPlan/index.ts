684e328fc7fa3be27007e300a155910fbbe1babc
Kanadets <dmitriifomin.dev@outlook.com>
2025-11-26 13:53:29 -0800

    clean up, preparing for work

--- modules/ExpoRoomPlan/index.ts
+++ modules/ExpoRoomPlan/index.ts
@@ -2,4 +2,3 @@
 // and on native platforms to ExpoRoomPlanModule.ts
 export * from "./src/ExpoRoomPlan.types";
 export { default } from "./src/ExpoRoomPlanModule";
-export { default as ExpoRoomPlanView } from "./src/ExpoRoomPlanView";
--- modules/ExpoRoomPlan/src/ExpoRoomPlanModule.ts
+++ modules/ExpoRoomPlan/src/ExpoRoomPlanModule.ts
@@ -1,12 +1,8 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import { NativeModule, requireNativeModule } from "expo";
 
-import { ExpoRoomPlanModuleEvents } from './ExpoRoomPlan.types';
+import { ExpoRoomPlanModuleEvents } from "./ExpoRoomPlan.types";
 
-declare class ExpoRoomPlanModule extends NativeModule<ExpoRoomPlanModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
-}
+declare class ExpoRoomPlanModule extends NativeModule<ExpoRoomPlanModuleEvents> {}
 
 // This call loads the native module object from the JSI.
-export default requireNativeModule<ExpoRoomPlanModule>('ExpoRoomPlan');
+export default requireNativeModule<ExpoRoomPlanModule>("ExpoRoomPlan");
